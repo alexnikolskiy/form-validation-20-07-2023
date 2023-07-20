fbdd3435e9725ea77bf8fdbc9ff9677bf0bcc9d4
Alexander Nikolsky <alexander.nikolsky@kaspersky.com>
2023-07-20 18:54:46 +0300

    fix: config

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,7 @@ const validationConfig = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__button',
-  activeButtonClass: 'popup__button_valid',
-  inactiveButtonClass: 'popup__button_invalid',
+  disabledButtonClass: 'popup__button_disabled',
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__input-error_visible'
 };
